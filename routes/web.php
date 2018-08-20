be4186f638ed9583c0be2cd03d011e60d6fcf16d
LinuxIfrs <victor.caraffini@ibiruba.ifrs.edu.br>
2018-08-20 16:29:11 -0300

    aula20-08

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,23 @@ Route::get('/', function () {
 });
 
 Route::get('/atividades', 'AtividadeController@index');
+Route::get('/atividades/create', 'AtividadeController@create');
+Route::post('/atividades', 'AtividadeController@store');
+Route::get('/atividades/{id}', 'AtividadeController@show');
+Route::get('/atividades/{id}/edit', 'AtividadeController@edit');
+Route::put('/atividades/{id}', 'AtividadeController@update');
+Route::get('/atividades/{id}/delete', 'AtividadeController@delete');
+Route::delete('/atividades/{id}', 'AtividadeController@destroy');
+
+
+Route::get('/mensagem', 'MensagemController@index');
+Route::get('/mensagem/{id}', 'MensagemController@show');
+
+Route::get('/mensagem/create', 'MensagemController@create');
+Route::post('/mensagem', 'MensagemController@store');
+
+Route::get('/mensagem/{id}/edit', 'MensagemController@edit');
+Route::put('/mensagem/{id}', 'MensagemController@update');
+Route::get('/mensagem/{id}/delete', 'MensagemController@delete');
+Route::delete('/mensagem/{id}', 'MensagemController@destroy');
 
